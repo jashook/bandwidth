29e07a9a35c9cd24696a8113ed100414c2728080
jashook <jshook@smu.edu>
2015-01-05 19:44:45 +0100

    passing all tests, need to check domain lookup and cross machine connections.  Around 5% of the time connect will throw an error.  Assuming it is not my code that is causing the problem

--- include/socket.hpp
+++ include/socket.hpp
@@ -23,6 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -31,10 +32,12 @@
 #include <string>
 #include <vector>
 
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,43 +56,79 @@ class socket
 {
    public:  // Constructor | Destructor
 
-      socket() { _ctor(); }
+      socket(std::size_t port) { _ctor(port); }
+      socket(const char* const ip, std::size_t port) { _ctor(port, ip); }
+      socket(const std::string& host_name, std::size_t port) { _ctor(port, host_name); }
       ~socket() { _dtor(); }
 
    public:  // Public member functions
 
       void accept() { _accept(); }
       void bind() { _bind(); }
+      void close() { _close(); }
+      void connect() { _connect(); }
       void listen() { _listen(); }
       void read(std::vector<char>& buffer) { _read(buffer); }
+      void read_back(std::vector<char>& buffer) { _read_back(buffer); }
       void write(const char* const message) { _write(message); }
       void write(const std::string& message) { _write(message); }
+      void write_back(const char* const message) { _write_back(message); }
+      void write_back(const std::string& message) { _write_back(message); }
 
    private: // Private member functions
 
       void _accept()
       {
-         socklen_t client_length = sizeof(_m_client_address);
+         socklen_t client_length = sizeof(sockaddr_in);
 
          _m_accepted_fd = ::accept(_m_socket_fd, (sockaddr *)&_m_client_address, &client_length);
 
          // -1 on failure
          if (_m_accepted_fd < 0)
          {
-            throw std::runtime_error("Unable to connect to socket.");
+            std::string err = "Unable to connect to socket - error number: ";
+            
+            err += errno;
+            
+            err += " -- ";
+            
+            err += strerror(errno);
+            
+            throw std::runtime_error(err);
          }
       }
 
       void _bind()
       {
-         if (::bind(_m_socket_fd, (const sockaddr*)&_m_server_address, sizeof(_m_server_address) < 0))
+         auto return_value = ::bind(_m_socket_fd, (const sockaddr*)&_m_server_address, sizeof(_m_server_address));
+         
+         if (return_value != 0)
          {
             throw std::runtime_error("Unable to bind to port");
          }
       }
-
-      void _ctor()
+   
+      void _ctor(std::size_t port, const std::string& host_name)
       {
+         hostent* host_ent;
+         
+         host_ent = ::gethostbyname(host_name.c_str());
+         
+         _ctor(port, host_ent->h_name);
+      }
+
+      void _ctor(std::size_t port, const char* const ip = nullptr)
+      {
+         _m_port_number = port;
+         
+         _m_ip_address = nullptr;
+         
+         if (ip != nullptr)
+         {
+            _m_ip_address = new char[std::strlen(ip) + 1];
+            std::strcpy(_m_ip_address, ip);
+         }
+         
          // AF_INET: Internet domain of the computer
          // SOCK_STREAM: TCP as opposed to UDP
          // 0: Choose TCP for the transfer protocol
@@ -101,8 +140,8 @@ class socket
             throw std::runtime_error("Unable to open socket.");
          }
 
-         memset(&_m_server_address, 0, sizeof(_m_server_address));
-         memset(&_m_client_address, 0, sizeof(_m_client_address));
+         memset(&_m_server_address, 0, sizeof(sockaddr_in));
+         memset(&_m_client_address, 0, sizeof(sockaddr_in));
 
          _m_server_address.sin_family = AF_INET;
          _m_server_address.sin_addr.s_addr = INADDR_ANY;
@@ -111,11 +150,42 @@ class socket
          _m_accepted_fd = 0;
          _m_amount_read = 0;
       }
+   
+      void _close()
+      {
+         ::close(_m_socket_fd);
+      }
+   
+      void _connect()
+      {
+         // IPv4
+         _m_server_address.sin_family = AF_INET;
+         _m_server_address.sin_port = _m_port_number;
+         
+         if (_m_ip_address != nullptr)
+         {
+            int status = ::inet_pton(AF_INET, _m_ip_address, &_m_server_address.sin_addr);
+            
+            if (status < 0)
+            {
+               throw std::runtime_error("Error cannot convert the ip address");
+            }
+         }
+         
+         auto return_value = ::connect(_m_socket_fd, (struct sockaddr *) &_m_server_address, sizeof(_m_server_address));
+         
+         if (return_value < 0)
+         {
+            throw std::runtime_error("Error cannot connect to the address");
+         }
+
+      }
 
       void _dtor()
       {
-         if (_m_accepted_fd) close(_m_accepted_fd);
-         if (_m_socket_fd) close(_m_socket_fd);
+         if (_m_accepted_fd) close();
+         if (_m_socket_fd) close();
+         if (_m_ip_address) delete _m_ip_address;
       }
 
       void _listen()
@@ -123,32 +193,53 @@ class socket
          // 5 is the max amount of waiting connections
          ::listen(_m_socket_fd, 5);
       }
-
+   
       void _read(std::vector<char>& buffer)
       {
          do
          {
             memset(_m_buffer, 0, sizeof(char) * BUFFER_SIZE);
             
-            _m_amount_read = ::read(_m_accepted_fd, _m_buffer, BUFFER_SIZE - 1);
+            _m_amount_read = (int)::read(_m_accepted_fd, _m_buffer, BUFFER_SIZE - 1);
 
             if (_m_amount_read < 0)
             {
                throw std::runtime_error("Error reading from the connection");
             }
 
-            for(int index = 0; index < _m_amount_read; ++_m_amount_read)
+            for(int index = 0; index < _m_amount_read; ++index)
             {
                buffer.push_back(_m_buffer[index]);
             }
 
          } while (_m_amount_read == BUFFER_SIZE - 1);
       }
-
+   
+      void _read_back(std::vector<char>& buffer)
+      {
+         do
+         {
+            memset(_m_buffer, 0, sizeof(char) * BUFFER_SIZE);
+            
+            _m_amount_read = (int)::read(_m_socket_fd, _m_buffer, BUFFER_SIZE - 1);
+            
+            if (_m_amount_read < 0)
+            {
+               throw std::runtime_error("Error reading from the connection");
+            }
+            
+            for(int index = 0; index < _m_amount_read; ++index)
+            {
+               buffer.push_back(_m_buffer[index]);
+            }
+            
+         } while (_m_amount_read == BUFFER_SIZE - 1);
+      }
+   
       void _write(const char* const message)
       {
-         int error_code = ::write(_m_accepted_fd, message, (std::size_t)strlen(message));
-
+         auto error_code = ::write(_m_socket_fd, message, (std::size_t)strlen(message));
+      
          if (error_code < 0)
          {
             throw std::runtime_error("Error writing to the connection");
@@ -157,7 +248,7 @@ class socket
 
       void _write(const std::string& message)
       {
-         int error_code = ::write(_m_accepted_fd, message.c_str(), message.size());
+         auto error_code = ::write(_m_socket_fd, message.c_str(), message.size());
 
          if (error_code < 0)
          {
@@ -165,9 +256,30 @@ class socket
          }
       }
 
+      void _write_back(const char* const message)
+      {
+         auto error_code = ::write(_m_accepted_fd, message, (std::size_t)strlen(message));
+         
+         if (error_code < 0)
+         {
+            throw std::runtime_error("Error writing to the connection");
+         }
+      }
+   
+      void _write_back(const std::string& message)
+      {
+         auto error_code = ::write(_m_accepted_fd, message.c_str(), message.size());
+         
+         if (error_code < 0)
+         {
+            throw std::runtime_error("Error writing to the connection");
+         }
+      }
+   
    private: // Member Variables
 
       char _m_buffer[BUFFER_SIZE];
+      char* _m_ip_address;
 
       int _m_accepted_fd;
       int _m_socket_fd;
--- test/socket_test.cpp
+++ test/socket_test.cpp
@@ -20,28 +20,235 @@
 #include "socket.hpp"
 #include "test.hpp"
 
+#include <chrono>
+#include <thread>
+#include <vector>
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-void test_socket_constructor()
+void test_socket_ctor_dtor()
 {
    ev9::socket* socket;
    
    try
    {
-      //socket = new ev9::socket();
+      socket = new ev9::socket(7000);
+      
+      socket->bind();
+      
+      delete socket;
    }
    
    catch (...)
    {
-      throw std::runtime_error(TEST_INFORMATION + std::string("unable to constructor socket"));
+      throw std::runtime_error(TEST_INFORMATION + std::string("unable to finish socket constructor"));
+   }
+}
+
+void test_accepting_connection()
+{
+   ev9::socket* socket;
+   
+   try
+   {
+      socket = new ev9::socket(7000);
+ 
+      socket->bind();
+      socket->listen();
+      socket->accept();
+      socket->accept();
+      
+      delete socket;
+   }
+   
+   catch (...)
+   {
+      throw std::runtime_error(TEST_INFORMATION);
+   }
+}
+
+void test_connect()
+{
+   ev9::socket* socket;
+   
+   // Sleep to allow the accept thread to finish.
+   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   
+   try
+   {
+      socket = new ev9::socket(7000);
+      
+      socket->connect();
+      
+      delete socket;
+   }
+   
+   catch (...)
+   {
+      throw std::runtime_error(TEST_INFORMATION);
+   }
+}
+
+void test_connect_different_ip()
+{
+   ev9::socket* socket;
+   
+   // Sleep to allow the accept thread to finish.
+   std::this_thread::sleep_for(std::chrono::milliseconds(15));
+   
+   try
+   {
+      socket = new ev9::socket("192.168.1.102", 7000);
+      
+      socket->connect();
+      
+      delete socket;
+      
+   }
+   
+   catch (...)
+   {
+      throw std::runtime_error(TEST_INFORMATION);
+   }
+   
+}
+
+void test_read()
+{
+   ev9::socket* socket;
+   
+   try
+   {
+      socket = new ev9::socket(7001);
+      
+      socket->bind();
+      socket->listen();
+      socket->accept();
+      
+      std::vector<char> input;
+      
+      socket->read(input);
+      
+      if (input.at(0) != 'a')
+      {
+         throw std::runtime_error(TEST_INFORMATION + "input does not equal a");
+      }
+   }
+   
+   catch (std::runtime_error& error)
+   {
+      throw std::runtime_error(error.what());
+   }
+
+}
+
+void test_write()
+{
+   ev9::socket* socket;
+   
+   // Sleep to allow the accept thread to finish.
+   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   
+   try
+   {
+      socket = new ev9::socket(7001);
+      
+      socket->connect();
+      
+      socket->write("a");
+      
+      delete socket;
+   }
+   
+   catch (...)
+   {
+      throw std::runtime_error(TEST_INFORMATION);
+   }
+}
+
+void read_write_setup()
+{
+   ev9::socket* socket;
+   
+   // Sleep to allow the accept thread to finish.
+   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   
+   try
+   {
+      socket = new ev9::socket(7002);
+      
+      socket->bind();
+      socket->listen();
+      socket->accept();
+      
+      std::vector<char> input_vector;
+      
+      socket->read(input_vector);
+      
+      socket->write_back("back");
+      
+      delete socket;
+   }
+   
+   catch (...)
+   {
+      throw std::runtime_error(TEST_INFORMATION);
+   }
+}
+
+void test_read_write()
+{
+   ev9::socket* socket;
+   
+   try
+   {
+      socket = new ev9::socket(7002);
+      
+      std::thread connecting_thread(read_write_setup);
+      
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+      
+      socket->connect();
+      
+      socket->write("a");
+      
+      std::vector<char> input_vector;
+      
+      socket->read_back(input_vector);
+      
+      std::string str;
+      
+      for (int index = 0; index < 4; ++index)
+      {
+         str.push_back(input_vector.at(index));
+      }
+  
+      if (str != "back")
+      {
+         throw 0;
+      }
+      
+      connecting_thread.join();
+      
+      delete socket;
+   }
+   
+   catch (...)
+   {
+      throw std::runtime_error(TEST_INFORMATION);
    }
 }
 
 int main()
 {
-   ev9::test socket_test(2);
+   ev9::test socket_test(0);
     
-   socket_test.add_test(test_socket_constructor);
-    
+   socket_test.add_test(test_socket_ctor_dtor);
+   socket_test.add_test(test_accepting_connection);
+   socket_test.add_test(test_connect);
+   socket_test.add_test(test_read);
+   socket_test.add_test(test_connect_different_ip);
+   socket_test.add_test(test_write);
+   socket_test.add_test(test_read_write);
 }
